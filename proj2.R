302d65cb3bbfdb019d396d0aed8b1954eccd10dc
Laura111111 <114145516+Laura111111@users.noreply.github.com>
2022-10-20 16:53:15 +0100

    Add files via upload

--- proj2.R
+++ proj2.R
@@ -3,16 +3,25 @@
 # contribution:
 
 
+
+
 # conduct a simulation with strategy 1 or 2 or 3 to test whether the prisoner k succeed to find the card k
+
+# define a function Strategy
+
+# input: 
+# 2*n means the number of prisoners
+# k means the selected prisoner's number
+# strategy space is {1,2,3}
+# strategy 1 represents that the prisoner k starts at the box with the number k on it, and when the card number inside this box is not k, then open the box with the number same as the card number inside the previous box, repeat this process until find the card with number k 
+# strategy 2 represents that the prisoner k starts at a randomly selected box, then repeat the process as strategy 1.
+# strategy 3 represents that the prisoner k open n boxes randomly to find the card with number k on it.
+# Card means a 2*n by 1 vector, its index is the box number, its element is the card number
+
+# output: 
 # if the prisoner k is successful to find the card k, the function Strategy will return 1. Otherwise, return 0
+
 Strategy = function(n, k, strategy, Card){
-  # 2*n is the total number of prisoners
-  # k is the selected prisoner's number
-  # strategy space is {1,2,3}
-  # strategy 1 represents that the prisoner k starts at the box with the number k on it, and when the card number inside this box is not k, then open the box with the number same as the card number inside the previous box, repeat this process until find the card with number k 
-  # strategy 2 represents that the prisoner k starts at a randomly selected box, then repeat the process as strategy 1.
-  # strategy 3 represents that the prisoner k open n boxes randomly to find the card with number k on it.
-  # Card is a 2*n by 1 vector, its index is the box number, its element is the card number
   # i.e. Card[i] = j, i is the index of box, j is the card number in that box
   Count = 0 
   # When the simulation is successful, the count is assigned to 1, otherwise, the count is still 0
@@ -36,7 +45,7 @@ Strategy = function(n, k, strategy, Card){
     }
     if (Card[Box_index] == k && trials <= n) {# only if the prisoner succeeds in finding the card in times
       Count = 1
-      # only if the prisoner find its number, say k, the Count is assigned to 1
+      # only if the prisoner finds its number, say k, the Count is assigned to 1
     }
   }else{
     Box_index = sample(Box, n) 
@@ -52,11 +61,24 @@ Strategy = function(n, k, strategy, Card){
   return(Count)
 }
 
-# estimate the probability of a single prisoner succeeding in finding the corresponding number.
+
+
+
+# estimate the probability of a single prisoner succeeding in finding the corresponding number
+
+# define a function Pone
+
+# input：
+# 2*n means the number of prisoners
+# k means the selected prisoner's number
+# strategy space is {1,2,3}，and the representation of the strategy is as the same as that in the function Strategy
+# nreps is the number of replicate simulations to run to estimate the the probability, 1000 is reasonable
+
+# output:
+# return Probability_Estimate, which means the probability of a single prisoner succeeding in finding their number
+
+
 Pone = function(n, k, strategy, nreps){
-  # n is the number of prisoners, k the prisoner's number,
-  # strategy 1 2 3 
-  # nreps is the number of replicate simulations to run to estimate the the probability, 1000 is reasonable
   Box <- array(1:(2*n))
   # create the 1 by 2*n boxes
   Success = 0
@@ -67,19 +89,32 @@ Pone = function(n, k, strategy, nreps){
     # shuffle the n cards and distribute them into the n boxes
     # Card[i] = j, i is the index of box, j is the card number in that box
     Success = Success + Strategy(n, k, strategy, Card)
-    
   }  
   Probability_Estimate = Success / nreps
   return(Probability_Estimate)
-  
 }
+
+
+
+
+# estimate the probability of all prisoners succeeding in finding the corresponding number
+
+# define a function Pall
+
+# input:
+# 2*n means the number of prisoners
+# strategy space is {1,2,3}，and the representation of the strategy is as the same as that in the function Strategy
+# nreps is the number of replicate simulations to run to estimate the the probability, 1000 is reasonable
+
+# output:
+# return Probability_Estimate, which means the probability of all prisoners finding their number
+
 
 Pall  = function(n, strategy, nreps) {
   Success = 0
   trials = 1
   Box <- array(1:(2*n))
   while (trials <= nreps){
-    
     # define 1 by 2n boxes which have a card of its number inside it, i.e. the ith box has a card with number i
     # shuffle the n cards and distribute them into the n boxes
     # Card[i] = j, i is the index of box, j is the card number in that box
@@ -104,29 +139,38 @@ Pall  = function(n, strategy, nreps) {
 
 
 # use the examples when n=5 and when n=50
+# use the function Strategy, function Pone, function Pall to get the result of examples
 # then estimate the probability of a single prisoner and all prosoners finding their number
 # get the success probabilities under each strategy
 
+# input:
+# n=5, strategy = 1 or 2 or 3, nreps = 10000, take a random k-value such as 2
+# n=50, strategy = 1 or 2 or 3, nreps = 10000, take a random k-value such as 2
+
+# output:
+# the individual and joint success probabilities under each strategy for n=5 and for n=50
+
+
 # example: strategy 1, n=5
-individual_1_5 <- paste("When strategy=1,n=5, the probability of a single prisoner succeeding in finding their number is:",Pone(5,2,1,10000),sep=" ")
+individual_1_5 <- paste("When strategy=1, n=5, the probability of a single prisoner succeeding in finding their number is:",Pone(5,2,1,10000),sep=" ")
 print(individual_1_5)
-joint_1_5 <- paste("When strategy=1,n=5, the probability of all prisoners finding their number is :",Pall(5,1,10000),sep=" ")
+joint_1_5 <- paste("When strategy=1, n=5, the probability of all prisoners finding their number is :",Pall(5,1,10000),sep=" ")
 print(joint_1_5)
 
 # example: strategy 2, n=5
-individual_2_5 <- paste("When strategy=2,n=5, the probability of a single prisoner succeeding in finding their number is:",Pone(5,2,2,10000),sep=" ")
+individual_2_5 <- paste("When strategy=2, n=5, the probability of a single prisoner succeeding in finding their number is:",Pone(5,2,2,10000),sep=" ")
 print(individual_2_5)
-joint_2_5 <- paste("When strategy=2,n=5, the probability of all prisoners finding their number is :",Pall(5,2,10000),sep=" ")
+joint_2_5 <- paste("When strategy=2, n=5, the probability of all prisoners finding their number is :",Pall(5,2,10000),sep=" ")
 print(joint_2_5)
 
 # example: strategy 3, n=5
-individual_3_5 <- paste("When strategy=3,n=5, the probability of a single prisoner succeeding in finding their number is:",Pone(5,2,3,10000),sep=" ")
+individual_3_5 <- paste("When strategy=3, n=5, the probability of a single prisoner succeeding in finding their number is:",Pone(5,2,3,10000),sep=" ")
 print(individual_3_5)
-joint_3_5 <- paste("When strategy=3,n=5, the probability of all prisoners finding their number is :",Pall(5,3,10000),sep=" ")
+joint_3_5 <- paste("When strategy=3, n=5, the probability of all prisoners finding their number is :",Pall(5,3,10000),sep=" ")
 print(joint_3_5)
 
 # example: strategy 1, n=50
-individual_1_50 <- paste("When strategy=1,n=50, the probability of a single prisoner succeeding in finding their number is:",Pone(50,2,1,10000),sep=" ")
+individual_1_50 <- paste("When strategy=1, n=50, the probability of a single prisoner succeeding in finding their number is:",Pone(50,2,1,10000),sep=" ")
 print(individual_1_50)
 joint_1_50 <- paste("When strategy=1, n=50, the probability of all prisoners finding their number is :",Pall(50,1,10000),sep=" ")
 print(joint_1_50)
@@ -134,7 +178,7 @@ print(joint_1_50)
 # example: strategy 2, n=50
 individual_2_50 <- paste("When strategy=2, n=50, the probability of a single prisoner succeeding in finding their number is:",Pone(50,2,2,10000),sep=" ")
 print(individual_2_50)
-joint_2_50 <- paste("When strategy=2,n=50, the probability of all prisoners finding their number is :",Pall(50,2,10000),sep=" ")
+joint_2_50 <- paste("When strategy=2, n=50, the probability of all prisoners finding their number is :",Pall(50,2,10000),sep=" ")
 print(joint_2_50)
 
 # example: strategy 3, n=50
@@ -144,6 +188,8 @@ joint_3_50 <- paste("When strategy=3, n=50, the probability of all prisoners fin
 print(joint_3_50)
 
 
+
+
 # remarks on results:
 # Generally speaking, in strategy 1, the probability of all prisoners finding their number is always the greatest
 # When n is enough big, the joint success probabilities of strategy 2 and strategy 3 are approximately approaching 0 
@@ -151,11 +197,23 @@ print(joint_3_50)
 # When n is large enogh, counterintuitively, the joint success probabilities of strategy 1 is approximating to 0.3 instead of 0
 
 
+
+
 # dloop is the function to estimate the probability of each loop length from 1 to 2n occuring at least once in a randomly shuffing of cards to boxes by simulation.
 # The return of dloop is a 1 by 2n array, whose entry i represents the probability of i-length loop occuring at least once.
+
+# define a function dloop
+
+# input:
+# 2*n means the number of prisoners
+# nreps is the number of replicate simulations to run to estimate the the probability, 1000 is reasonable
+
+# output:
+# return(colSums(frequency)/nreps)
+# the value of which means the probability of each loop length from 1 to 2*n occurring at least in a random shuffling of cards to boxes
+
+
 dloop = function(n, nreps){
-  # 2n is the number of prisoners.
-  # nreps is the number of simulations. 
   # frequency is a nreps by 2n array used to record the different length loops' occuring probabilities
   frequency = matrix(0,nrow=nreps,ncol=2*n) 
   for (i in 1:nreps){
@@ -184,6 +242,5 @@ prob = dloop(50,10000)
 print(prob)
 result <- paste("The probability that there is no loop longer than 50 in a random reshuffling cards to boxes:",1-sum(prob[51:100]),sep=" ")
 print(result)
-#visualise the probabilities
+# visualise the probabilities
 plot(prob)
-
